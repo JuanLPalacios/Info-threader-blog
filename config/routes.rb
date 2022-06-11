5ad4ddefbb097735a3132fe7ed0340f7ffd2ee73
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-06-11 11:00:57 -0500

    Add controlers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'users#index'
+  get 'users/:user_id/posts(.:format)',to: 'posts#index'
+  get 'users/:user_id/posts/:id(.:format)',to: 'posts#show'
+  get 'users(.:format)',to: 'users#index'
+  get 'users/:id(.:format)',to: 'users#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
