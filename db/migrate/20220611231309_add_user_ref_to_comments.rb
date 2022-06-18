480fd964f64249760dcc4d9ed9a881f0c0ba3e4c
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-06-18 10:52:48 -0500

    Fix wrong author fk name

--- db/migrate/20220611231309_add_user_ref_to_comments.rb
+++ db/migrate/20220611231309_add_user_ref_to_comments.rb
@@ -1,5 +1,5 @@
 class AddUserRefToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :user, column: :author_id, index: true, foreign_key: true
-    end
+    add_foreign_key :comments, :users, column: :author_id
+  end
 end
--- db/migrate/20220611231411_add_user_ref_to_posts.rb
+++ db/migrate/20220611231411_add_user_ref_to_posts.rb
@@ -1,5 +1,5 @@
 class AddUserRefToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :user, column: :author_id, index: true, foreign_key: true
+    add_foreign_key :posts, :users, column: :author_id
   end
 end
