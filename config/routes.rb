b825154a27eeaa2f04c3c6f7f19ade0f636507d5
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-06-18 16:06:15 -0500

    Add view structure for users show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get 'users/:user_id/posts(.:format)',to: 'posts#index'
-  get 'users/:user_id/posts/:id(.:format)',to: 'posts#show'
+  get 'users/:user_id/posts(.:format)',to: 'posts#index', as: :posts
+  get 'users/:user_id/posts/:id(.:format)',to: 'posts#show', as: :show_post
   get 'users(.:format)',to: 'users#index'
   get 'users/:id(.:format)',to: 'users#show', as: :show_user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
