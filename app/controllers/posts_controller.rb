2bd70e65387dc55ca8ef4ac0f6d070d830ac7afe
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-06-18 17:30:10 -0500

    Add  posts index view structure

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    params['page'] = 1 if (params['page'].nil?) || ((params['page'].to_i < 1))
+    @user = User.find(params['user_id'].to_i)
+    @posts = @user.posts.limit(2).offset((params['page'].to_i - 1)*2)
+    @page = params['page'].to_i
+  end
 
   def show; end
 end
