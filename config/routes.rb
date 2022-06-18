6d7197e346e149ad41d100d1ec24dc696740ecf7
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-06-18 13:58:10 -0500

    Add user index template structure

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'users/:user_id/posts(.:format)',to: 'posts#index'
   get 'users/:user_id/posts/:id(.:format)',to: 'posts#show'
   get 'users(.:format)',to: 'users#index'
-  get 'users/:id(.:format)',to: 'users#show'
+  get 'users/:id(.:format)',to: 'users#show', as: :show_user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
