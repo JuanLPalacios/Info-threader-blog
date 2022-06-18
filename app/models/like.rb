4f1f8334d534f24893bd2a65325d9ca84f2a6fa7
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-06-18 11:15:31 -0500

    Fix linters

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    def self.update_post_likes_counter(post)
-        post.update(likes_counter: Like.where(post_id: post.id).length)
-    end
+  def self.update_post_likes_counter(post)
+    post.update(likes_counter: Like.where(post_id: post.id).length)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,13 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    has_many :comments
-    has_many :likes
-    
-    def self.update_user_posts_counter(user)
-        user.update(post_counter: Post.where(author_id: user.id).length)
-    end
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+  has_many :likes
 
-    def most_recent_comments
-      self.comments.order(created_at: :desc).limit(5)
-    end
+  def self.update_user_posts_counter(user)
+    user.update(post_counter: Post.where(author_id: user.id).length)
+  end
+
+  def most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
