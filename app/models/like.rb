a89f345cc3a454c31507064ec701a2fe2be4aeb4
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-06-18 12:14:34 -0500

    Remove param post from update_post_likes_counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def self.update_post_likes_counter(post)
-    post.update(likes_counter: Like.where(post_id: post.id).length)
+  def update_likes_counter
+    post.increment!(:likes_counter)
   end
 end
