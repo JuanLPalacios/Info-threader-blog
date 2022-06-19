f7fe654d85694bde6ca802fd410662b77316cee0
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-06-18 22:13:49 -0500

    Add like button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,20 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params['id'].to_i)
   end
+
+  def create
+    render "show"
+  end
+
+  def comment
+    render "show"
+  end
+
+  def like
+    @post = Post.find(params['id'].to_i)
+    like = Like.create(author: ApplicationRecord.current_user, post_id: params['id'].to_i)
+    like.update_likes_counter
+    redirect_to show_post_path(@post.author, @post)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'users/:user_id/posts/:id(.:format)',to: 'posts#show', as: :show_post
   get 'users(.:format)',to: 'users#index'
   get 'users/:id(.:format)',to: 'users#show', as: :show_user
+  post 'posts/:id/likes(.:format)',to: 'posts#like', as: :like
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
