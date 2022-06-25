2203f8be327066cbf580612abf9e1b5e28e5a042
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-06-25 18:35:42 -0500

    Add like unit tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'update_likes_counter method for a given post:' do
+    it 'Should update the post likes_counter' do
+      like = Like.create(author: User.first, post: Post.first)
+      like.update_likes_counter
+
+      expect(Post.first.likes_counter).to eq(Post.first.likes.length)
+    end
+  end
 end
