d2b2cb3b8ab1654e32d9275b96336fc45dfaed2f
JuanLPalacios <juan.luis.palacios.p@gmail.com>
2022-07-02 20:35:20 -0500

    Add api coments index and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-Rails.application.routes.draw do
-    devise_for :users,
-               controllers: {
-                   sessions: 'api/sessions'
-               }, as: :api_users
-    # ...
-  end
+  devise_for :users,
+              path: 'api',
+              controllers: {
+                  sessions: 'users/sessions'
+              }, as: :api_users
   root 'users#index'
   get 'users/:user_id/posts(.:format)',to: 'posts#index', as: :posts
   get 'users/:user_id/posts/:id(.:format)',to: 'posts#show', as: :show_post
@@ -18,6 +16,10 @@ Rails.application.routes.draw do
   delete 'comments/:id(.:format)',to: 'comments#delete', as: :delete_comment
   get 'posts/new'
   post 'posts/create'
+  get 'api/users/:author_id/posts(.:format)',to: 'api/posts#index'
+  get 'api/users/:author_id/posts/:id/comments(.:format)',to: 'api/comments#index'
+  post 'api/users/:author_id/posts/:id/comments(.:format)',to: 'api/comments#create'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
